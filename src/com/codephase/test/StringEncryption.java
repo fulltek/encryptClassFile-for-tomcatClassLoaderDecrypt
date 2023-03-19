9906225b180d2f4f2751babb952b1398a0808aba
MarkOnTheWay <xanjun_yan@yahoo.com>
2023-03-19 12:01:40 +0800

    Update StringEncryption.java

--- src/com/codephase/test/StringEncryption.java
+++ src/com/codephase/test/StringEncryption.java
@@ -5,10 +5,9 @@ import com.codephase.tool.EncryptUtil;
 public class StringEncryption {
 
 	public static void main(String[] args) throws Exception{
-		String name="elearning";
-		String password="Codephasegd1302";
+		String name="databaseName";
+		String password="databasePassword";
 		System.out.println("数据库用户名： "+EncryptUtil.aesEncryptFromStringToBase64String(name));
 		System.out.println("数据库密码： "+EncryptUtil.aesEncryptFromStringToBase64String(password));
-	   //oXOLuNgIpiK9semkMbCQbQ==------H20tYwjXQ2C/c+Kiqykumg==
 	}
 }
