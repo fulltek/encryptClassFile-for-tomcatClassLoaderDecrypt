bbfef686fa72c2c4ef763aa8e0bb0e48a94efad2
MarkOnTheWay <xanjun_yan@yahoo.com>
2023-03-19 12:02:37 +0800

    Update StringDecryption.java

--- src/com/codephase/test/StringDecryption.java
+++ src/com/codephase/test/StringDecryption.java
@@ -4,8 +4,8 @@ import com.codephase.tool.EncryptUtil;
 
 public class StringDecryption {
 	public static void main(String[] args) throws Exception{
-		String encryptedName="oXOLuNgIpiK9semkMbCQbQ==";
-		String encryptedPassword="H20tYwjXQ2C/c+Kiqykumg==";
+		String encryptedName="encryptedName";
+		String encryptedPassword="encryptedPassword";
 		System.out.println(EncryptUtil.aesDecryptToStringByBase64String(encryptedName)+"~~~~~~"+EncryptUtil.aesDecryptToStringByBase64String(encryptedPassword));
 	}
 }
